d5e8ab1debe211968242f4c903ad550cf1dc64f7
Alex Yang <zhiyan@microsoft.com>
2019-10-21 22:17:48 +0000

    Merged PR 2164648: Add `GeoMatch` unit tests to `customrule`
    In this PR:
    * Added unit tests to `GeoMatch` with `RemoteAddr` as match variable
    * Added unit tests to `GeoMatch` with `RequestHeaders:X-Forwarded-For` as match variable
    
    PR URL: https://msazure.visualstudio.com/DefaultCollection/One/_git/Networking-Azwaf/pullrequest/2164648
    
    Related work items: #5584833

---
+++ customrule/eval_geomatch_test.go
@@ -0,0 +1,163 @@
+package customrule
+
+import (
+	"azwaf/testutils"
+	"azwaf/waf"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var geoMatchRemoteAddrBlockRule = &mockCustomRule{
+	name:     "geoMatchXForwardedForBlockRule",
+	priority: 1,
+	ruleType: "MatchRule",
+	matchConditions: []waf.MatchCondition{
+		&mockMatchCondition{
+			matchVariables: []waf.MatchVariable{
+				&mockMatchVariable{
+					variableName: "RemoteAddr",
+				},
+			},
+			operator:        "GeoMatch",
+			negateCondition: false,
+			matchValues:     []string{"CC"},
+		},
+	},
+	action: "Block",
+}
+
+func TestGeoMatchRemoteAddrBlockRulePositive(t *testing.T) {
+	assert := assert.New(t)
+	logger := testutils.NewTestLogger(t)
+
+	// Arrange
+	req := &mockWafHTTPRequest{
+		uri:        "/",
+		method:     "GET",
+		remoteAddr: "2.2.2.2",
+	}
+
+	engine, _, err := newEngineWithCustomRules(geoMatchRemoteAddrBlockRule)
+	if err != nil {
+		t.Fatalf("Got unexpected error: %s", err)
+	}
+
+	// Act
+	eval := engine.NewEvaluation(logger, req)
+	defer eval.Close()
+	err = eval.ScanHeaders()
+	decision := eval.EvalRules()
+
+	// Assert
+	assert.Nil(err)
+	assert.Equal(waf.Block, decision)
+}
+
+func TestGeoMatchRemoteAddrBlockRuleNegative(t *testing.T) {
+	assert := assert.New(t)
+	logger := testutils.NewTestLogger(t)
+
+	// Arrange
+	req := &mockWafHTTPRequest{
+		uri:    "/",
+		method: "GET",
+		headers: []waf.HeaderPair{
+			&mockHeaderPair{k: "X-Forwarded-For", v: "2.2.2.2"},
+		},
+	}
+
+	engine, _, err := newEngineWithCustomRules(geoMatchRemoteAddrBlockRule)
+	if err != nil {
+		t.Fatalf("Got unexpected error: %s", err)
+	}
+
+	// Act
+	eval := engine.NewEvaluation(logger, req)
+	defer eval.Close()
+	err = eval.ScanHeaders()
+	decision := eval.EvalRules()
+
+	// Assert
+	assert.Nil(err)
+	assert.Equal(waf.Pass, decision)
+}
+
+var geoMatchXForwardedForBlockRule = &mockCustomRule{
+	name:     "geoMatchXForwardedForBlockRule",
+	priority: 1,
+	ruleType: "MatchRule",
+	matchConditions: []waf.MatchCondition{
+		&mockMatchCondition{
+			matchVariables: []waf.MatchVariable{
+				&mockMatchVariable{
+					variableName: "RequestHeaders",
+					selector:     "X-Forwarded-For",
+				},
+			},
+			operator:        "GeoMatch",
+			negateCondition: false,
+			matchValues:     []string{"CC"},
+		},
+	},
+	action: "Block",
+}
+
+func TestGeoMatchXForwardedForBlockRulePositive(t *testing.T) {
+	assert := assert.New(t)
+	logger := testutils.NewTestLogger(t)
+
+	// Arrange
+	req := &mockWafHTTPRequest{
+		uri:    "/",
+		method: "GET",
+		headers: []waf.HeaderPair{
+			&mockHeaderPair{k: "X-Forwarded-For", v: "0.0.0.0,1.1.1.1:24601,2.2.2.2"},
+		},
+	}
+
+	engine, _, err := newEngineWithCustomRules(geoMatchXForwardedForBlockRule)
+	if err != nil {
+		t.Fatalf("Got unexpected error: %s", err)
+	}
+
+	// Act
+	eval := engine.NewEvaluation(logger, req)
+	defer eval.Close()
+	err = eval.ScanHeaders()
+	decision := eval.EvalRules()
+
+	// Assert
+	assert.Nil(err)
+	assert.Equal(waf.Block, decision)
+}
+
+func TestGeoMatchXForwardedForBlockRuleNegative(t *testing.T) {
+	assert := assert.New(t)
+	logger := testutils.NewTestLogger(t)
+
+	// Arrange
+	req := &mockWafHTTPRequest{
+		uri:        "/",
+		method:     "GET",
+		remoteAddr: "2.2.2.2",
+		headers: []waf.HeaderPair{
+			&mockHeaderPair{k: "X-Forwarded-For", v: "0.0.0.0,1.1.1.1"},
+		},
+	}
+
+	engine, _, err := newEngineWithCustomRules(geoMatchXForwardedForBlockRule)
+	if err != nil {
+		t.Fatalf("Got unexpected error: %s", err)
+	}
+
+	// Act
+	eval := engine.NewEvaluation(logger, req)
+	defer eval.Close()
+	err = eval.ScanHeaders()
+	decision := eval.EvalRules()
+
+	// Assert
+	assert.Nil(err)
+	assert.Equal(waf.Pass, decision)
+}
